5db559f531248fba5851266f08f3e1ae10f3f803
moonitime <thecollectivesofbest112@gmail.com>
2022-07-10 16:36:58 +0000

    lavender: Welcome RiceDroid!
    Signed-off-by: Moonitime: Ultra Romantic <moonitime@protonmail.com>

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -8,18 +8,24 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Arrow stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common RiceDroid stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+
+# Rom flags
+RICE_MAINTAINER := JojiOnThatBeat•Moonitime
+RICE_OFFICIAL := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_lavender
+PRODUCT_NAME := lineage_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
